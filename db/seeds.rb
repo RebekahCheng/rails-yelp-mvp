c4a93189c7c8ee8c53191097c344484d6d55a888
Rebekah Cheng <work@rebekahcheng.com>
2017-05-24 16:26:55 +0100

    added nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Restaurant.destroy_all
+
 5.times do
 
-  Restaurant.create(name: Faker::Name.name, address: Faker::Address.street_address, phone_number: Faker::Number.number(7), category: Restaurant::CATEGORIES.sample)
+  Restaurant.create(name: Faker::Food.ingredient, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone, category: Restaurant::CATEGORIES.sample)
 
 end
